5b45a121b8ad0b404a1e4aa7a64f13a360bfd659
Vladislav Cernega <vladislav@walkcat.net>
2023-01-28 21:23:53 +0000

    Commit on 2023-01-28 21:23:53

--- KeyEvents/script.js
+++ KeyEvents/script.js
@@ -1,20 +1,9 @@
 const insert = document.getElementById('insert')
 
 window.addEventListener('keydown', (event) => {
-  insert.innerHTML = `
-  <div class="key">
-  ${event.key === ' ' ? 'Space' : event.key} 
-  <small>event.key</small>
-</div>
-
-<div class="key">
-  ${event.keyCode}
-  <small>event.keyCode</small>
-</div>
-
-<div class="key">
-  ${event.code}
-  <small>event.code</small>
-</div>
-  `
+    insert.textContent = `
+    ${event.key === ' ' ? 'Space' : event.key} (event.key)
+    ${event.keyCode} (event.keyCode)
+    ${event.code} (event.code)
+    `
 })
